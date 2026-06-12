262266b31218d8066fdaf73f431fdf17cfb95346
jerryofficial402-hash <jerryofficial402@gmail.com>
2026-06-12 17:49:27 -0400

    Rebuild Tailwind CSS to include new color classes (#15803d green, #e31837 red) and add blog/dashboard content scan paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: [
     "./*.html",
+    "./blog/**/*.html",
+    "./dashboard/**/*.html",
     "./routes/**/*.html",
     "./services/**/*.html",
     "./quote/**/*.html"
